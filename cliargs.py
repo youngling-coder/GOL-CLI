2e7cb04686dd555041852ef80fa922cd8db9a653
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-08-01 19:01:53 +0200

    add some user flags to customize widget's work

---
+++ cliargs.py
@@ -0,0 +1,6 @@
+class CLIArg:
+    Height = "-H"
+    Width = "-W"
+    Character = "-c"
+    TerminalSize = "-t"
+    RefillOnResize = "-r"
--- main.py
+++ main.py
@@ -1,57 +1,80 @@
 import time
 import os
-from copy import deepcopy
+import sys
 import random
 import numpy as np
 from style import StyleConfig
-
-
-generation = 0
-
-total_births = 0
-total_deaths = 0
+from cliargs import CLIArg
 
 
 class ConwaysGameOfLife:
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        char: chr,
+        attach_to_terminal_size: bool = False,
+        refill_on_resize: bool = False,
+    ) -> None:
         self._width = width
         self._height = height
 
-        self._field = np.zeros((self._height, self._width))
-        self._new_frame = deepcopy(self._field)
+        self._refill_on_resize = refill_on_resize
+        print(refill_on_resize)
+        self._is_attached_to_terminal_size = attach_to_terminal_size
 
+        self._char = char
+
+        self._generation = 0
+
+        self._total_births = 0
+        self._total_deaths = 0
+
+        self._field = np.zeros((self._height, self._width))
+        self._new_frame = self._field.copy()
 
     def create_state(self):
         for y in range(self._height):
             for x in range(self._width):
                 self._field[y, x] = random.randint(0, 1)
 
-
     def update_size(self):
         terminal_size = os.get_terminal_size()
-    
+
         new_height = terminal_size.lines - 3
         new_width = terminal_size.columns - 2
-    
-        if new_height > self._height:
-            new_rows = np.zeros((new_height - self._height, self._width))
-            self._field = np.vstack([self._field, new_rows])
-            self._new_frame = np.vstack([self._new_frame, new_rows])
-        elif new_height < self._height:
-            self._new_frame = self._new_frame[:new_height]
-            self._field = self._field[:new_height]
+        size_icnreased = False
 
-        self._height = new_height
+        if self._is_attached_to_terminal_size or not self._height:
+            if new_height > self._height:
+                new_rows = np.zeros((new_height - self._height, self._width))
+                self._field = np.vstack([self._field, new_rows])
+                self._new_frame = np.vstack([self._new_frame, new_rows])
+                size_icnreased = True
+            elif new_height < self._height:
+                self._new_frame = self._new_frame[:new_height]
+                self._field = self._field[:new_height]
 
-        if new_width > self._width:
-            new_cols = np.zeros((self._height, new_width - self._width))
-            self._field = np.hstack([self._field, new_cols])
-            self._new_frame = np.hstack([self._new_frame, new_cols])
-        elif new_width <  self._width:
-            self._new_frame = np.delete(self._new_frame, list(range(new_width, self._width)), axis=1)
-            self._field = np.delete(self._field, list(range(new_width, self._width)), axis=1)
-        
-        self._width = new_width
+            self._height = new_height
+
+        if self._is_attached_to_terminal_size or not self._width:
+            if new_width > self._width:
+                new_cols = np.zeros((self._height, new_width - self._width))
+                self._field = np.hstack([self._field, new_cols])
+                self._new_frame = np.hstack([self._new_frame, new_cols])
+                size_icnreased = True
+            elif new_width < self._width:
+                self._new_frame = np.delete(
+                    self._new_frame, list(range(new_width, self._width)), axis=1
+                )
+                self._field = np.delete(
+                    self._field, list(range(new_width, self._width)), axis=1
+                )
+
+            self._width = new_width
+
+        if self._is_attached_to_terminal_size and self._refill_on_resize and size_icnreased:
+            self.create_state()
 
 
     def stay_alive(self, x: int, y: int):
@@ -70,53 +93,50 @@ class ConwaysGameOfLife:
             else:
                 self._new_frame[y, x] = 1
 
-
     def count_alive_cells_in_area(self, area: np.array):
-        
 
         count = np.count_nonzero(area)
 
         return count if not count else count
-            
-
 
     def get_area_around(self, x: int, y: int):
-        '''
+        """
         Calculates and returns 2D slice of the field around the cells with X and Y coordinates
         :param int x: X coordinate of the target cell
         :param int y: Y coordinate of the target cell
         :rtype: None
-        '''
+        """
 
         # Set default values for indexes, supposing X and Y don't have minimum or maximum values
-        x_subarea_indexes = [x-1, x, x+1]
-        y_subarea_indexes = [y-1, y, y+1]
+        x_subarea_indexes = [x - 1, x, x + 1]
+        y_subarea_indexes = [y - 1, y, y + 1]
 
         # If any of the conditions are not satisfied, set new X indexes
         if x == 0:
-            x_subarea_indexes = [self._width-1, x, x+1]
-        elif x == self._width-1:
-            x_subarea_indexes = [self._width-2, self._width-1, 0]
+            x_subarea_indexes = [self._width - 1, x, x + 1]
+        elif x == self._width - 1:
+            x_subarea_indexes = [self._width - 2, self._width - 1, 0]
 
         # The same with Y indexes
         if y == 0:
-            y_subarea_indexes = [self._height-1, y, y+1]
-        elif y == self._height-1:
-            y_subarea_indexes = [self._height-2, self._height-1, 0]
+            y_subarea_indexes = [self._height - 1, y, y + 1]
+        elif y == self._height - 1:
+            y_subarea_indexes = [self._height - 2, self._height - 1, 0]
 
         # Selecting values that are in range of X and Y indexes
         sub_area = self._field[np.ix_(y_subarea_indexes, x_subarea_indexes)]
-        
+
         return sub_area
 
-        
-
     def draw_field(self):
-
-        if self._width % 2 == 0:
-            frame_str = "╭" + "─" * int((self._width - 23) / 2) + " Conway's Game Of Life " + "─" * (int((self._width - 23) / 2)+1) + "╮\n"
-        else:
-            frame_str = "╭" + "─" * int((self._width - 23) / 2) + " Conway's Game Of Life " + "─" * int((self._width - 23) / 2) + "╮\n"
+        additional = int(not (self._width % 2))
+        frame_str = (
+            "╭"
+            + "─" * int((self._width - 23) / 2)
+            + " Conway's Game Of Life "
+            + "─" * (int((self._width - 23) / 2) + additional)
+            + "╮\n"
+        )
 
         for y in range(self._height):
             frame_str += f"{StyleConfig.BorderColor + StyleConfig.FontStyle}│{StyleConfig.ResetAll}"
@@ -124,10 +144,24 @@ class ConwaysGameOfLife:
                 if not self._new_frame[y, x]:
                     frame_str += " "
                 else:
-                    frame_str += f"{StyleConfig.AliveColor + StyleConfig.FontStyle + StyleConfig.BackgroundColor}"
+                    frame_str += f"{StyleConfig.AliveColor + StyleConfig.FontStyle + StyleConfig.BackgroundColor}{self._char}"
             frame_str += f"{StyleConfig.ResetAll + StyleConfig.BorderColor + StyleConfig.FontStyle + StyleConfig.BackgroundColor}│\n"
         frame_str += "╰" + "─" * self._width + "╯"
         print(frame_str)
+
+    @staticmethod
+    def show_help():
+        help_msg = f"""
+  {StyleConfig.FontStyle}{StyleConfig.BorderColor}  Welcome to Conway's Game Of Life!
+    {StyleConfig.AliveColor}
+    -h\t 󰜴 \tShow this message, discards program start
+
+    -H\t 󰜴 \tSet default height (0 value will be overrided by terminal height)
+    -W\t 󰜴 \tSet default width (0 value will be overrided by terminal width)
+    -c\t 󰜴 \tUse custom character for a cell (default is '')
+    -t\t 󰜴 \tUse terminal size (overrides -H and -W)
+    -r\t 󰜴 \tRefill the field on resize (work when -t flag specified)"""
+        print(help_msg)
 
     def run(self):
         self.create_state()
@@ -139,13 +173,43 @@ class ConwaysGameOfLife:
                 for x in range(self._width):
                     self.stay_alive(x, y)
 
-            self._field = deepcopy(self._new_frame)
+            self._field =  self._new_frame.copy()
 
-
-            time.sleep(.2)
+            time.sleep(0.2)
             os.system("clear")
 
 
-game = ConwaysGameOfLife(0, 0)
-game.update_size()
-game.run()
+if __name__ == "__main__":
+    args = sys.orig_argv
+    print(args)
+    height = 0
+    width = 0
+    char = ""
+    attach_to_terminal_size = False
+    refill_on_resize = False
+
+    if "-h" in args:
+        ConwaysGameOfLife.show_help()
+    else:
+        for i in range(1, len(args)):
+            if args[i] == CLIArg.Height:
+                height = int(args[i + 1])
+            elif args[i] == CLIArg.Width:
+                width = int(args[i + 1])
+            elif args[i] == CLIArg.Character:
+                char = args[i + 1]
+            elif args[i] == CLIArg.TerminalSize:
+                attach_to_terminal_size = True
+            elif args[i] == CLIArg.RefillOnResize:
+                refill_on_resize = True
+
+        game = ConwaysGameOfLife(
+            width=width,
+            height=height,
+            char=char,
+            attach_to_terminal_size=attach_to_terminal_size,
+            refill_on_resize=refill_on_resize,
+        )
+
+        game.update_size()
+        game.run()
